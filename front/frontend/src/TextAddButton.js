298204bdc71c27b40d0227450480f92652d9015d
knao31 <nao13.k.02@gmail.com>
2022-01-22 15:46:52 +0900

    watchwg-fetch

--- front/frontend/src/TextAddButton.js
+++ front/frontend/src/TextAddButton.js
@@ -1,17 +1,47 @@
 import { IconButton } from "@material-ui/core";
 import React from "react";
-
-const TextAddButton = () => {
-
-
+import SendIcon from '@material-ui/icons/Send';
+import 'whatwg-fetch';
 
 
+const TextAddButton = ({ text }) => {
+    // post request
+    const requestOptions = {
+        method: 'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({text: {text}})
+        };
+        fetch("/resive",requestOptions
+        //   resopnse
+      
+        ).then((response)=> response.json()
+        ).then((responseJson) =>{
+        console.log(responseJson)
+        }).catch(()=>{
+        console.log("error");
+        })
+
+
+    const clickSubmit = (e) => {
+        console.log("送信完了")
+        
+    };
+
+
+      
 
 
 
-    
+
     return (
-        <IconButton></IconButton>
+        <IconButton
+        onClick={() => {
+            console.log('送信完了');
+        }}
+        >
+
+        <SendIcon />
+        </IconButton>
     );
 };
 
