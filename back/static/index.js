a01c07054071408dee69ae491c1567d7dc838a3d
KotaYuno <84302413+KotaYuno@users.noreply.github.com>
2022-01-22 22:15:42 +0900

    post

--- back/static/index.js
+++ back/static/index.js
@@ -4,8 +4,14 @@ window.addEventListener("load", function () {
     button1.addEventListener("click", function () {
       const textbox = document.querySelector("#text");
       const textvalue = textbox.value;
-      const text_url = encodeURIComponent(textvalue);
-      fetch(`/resive/${text_url}`)
+      const param = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({ text: textvalue }),
+      };
+      fetch("/resive", param)
         .then((response) => response.text())
         .then((text_resive) => {
           document.querySelector("#add").innerHTML = text_resive;
