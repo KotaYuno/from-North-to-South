6cbd685e83becad051d5a85d34a85c138187e85e
LennyTGIF <skyzs.9097@gmail.com>
2022-01-22 18:45:07 +0900

    update

--- back/static/index.js
+++ back/static/index.js
@@ -1,23 +1,23 @@
-window.addEventListener('load', function(){
-    function initEventHandler(){
-        const button1 = document.querySelector("#button");
-        button1.addEventListener('click', function(){
-            const textbox = document.querySelector("#text");
-            const textvalue = textbox.value;
-            const param  = {
-                method: "POST",
-                headers: {
-                "Content-Type": "application/json; charset=utf-8"
-                },
-            body: JSON.stringify({text: textvalue})
-            }
-            fetch("/resive", param)
-                .then(response => response.text())
-                .then(text_resive => {
-                    document.querySelector("#add").innerHTML = text_resive;
-                })
-        })
-    }
+window.addEventListener("load", function () {
+  function initEventHandler() {
+    const button1 = document.querySelector("#button");
+    button1.addEventListener("click", function () {
+      const textbox = document.querySelector("#text");
+      const textvalue = textbox.value;
+      const param = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({ text: textvalue }),
+      };
+      fetch("/resive", param)
+        .then((response) => response.text())
+        .then((text_resive) => {
+          document.querySelector("#add").innerHTML = text_resive;
+        });
+    });
+  }
 
-    initEventHandler();
-})
+  initEventHandler();
+});
--- front/frontend/src/components/App.js
+++ front/frontend/src/components/App.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import Main from './Main';
-import SignIn from './SignIn';
-import config from '../config.json';
+import React, { useState } from "react";
+import Main from "./Main";
+import SignIn from "./SignIn";
+import config from "../config.json";
 
 export default () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-  if (config.signInEnabled && name === '') {
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
   }
-  
-  
 };
