908c2b6b15c4eca06febab48d0b63595c74d87be
LennyTGIF <skyzs.9097@gmail.com>
2022-01-23 01:11:44 +0900

    add fetch

--- front/src/components/MessageSubmitButton.js
+++ front/src/components/MessageSubmitButton.js
@@ -3,6 +3,7 @@ import { IconButton } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
 import { pushMessage } from '../firebase';
+import fetchData from '../hooks/fetchData'///+++
 
 const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
   return (
@@ -12,6 +13,8 @@ const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
         pushMessage({ name, text });
         setText('');
         inputEl.current.focus();
+        // alert(fetchData(text));///+++
+        console.log("aa",fetchData(text));///+++
       }}
     >
       <SendIcon />
